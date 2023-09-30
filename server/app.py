1e2398a87abcd741f72baa6c8a971accea880687
yisak <yisakbruck23@gmail.com>
2023-09-30 15:07:29 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -2,7 +2,6 @@
 
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
 
 from models import db, Plant
 
@@ -14,14 +13,27 @@ app.json.compact = True
 migrate = Migrate(app, db)
 db.init_app(app)
 
-api = Api(app)
+@app.route('/plants', methods=['GET'])
+def get_plants():
+    plants = Plant.query.all()
+    return jsonify([plant.to_dict() for plant in plants])
 
-class Plants(Resource):
-    pass
+@app.route('/plants/<int:id>', methods=['GET'])
+def get_plant_by_id(id):
+    plant = Plant.query.get_or_404(id)
+    return jsonify(plant.to_dict())
 
-class PlantByID(Resource):
-    pass
-        
+@app.route('/plants', methods=['POST'])
+def create_plant():
+    data = request.get_json()
+    new_plant = Plant(
+        name=data.get('name'),
+        image=data.get('image'),
+        price=data.get('price')
+    )
+    db.session.add(new_plant)
+    db.session.commit()
+    return jsonify(new_plant.to_dict()), 201
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
